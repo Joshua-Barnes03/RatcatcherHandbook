d9b7114308885e1898416432fc7b3d6f8fc20fb9
Joshua Barnes <josh.barnes003@gmail.com>
2021-11-03 15:49:13 -0400

    resource tracker basic functionality completed

--- src/Components/Resource.jsx
+++ src/Components/Resource.jsx
@@ -1,24 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Resource = function() {
+const [RESOURCE, START, INPUT, OUTPUT, DAYS, RESULT] = ['NAME', 'START', 'INPUT', 'OUTPUT', 'DAYS', 'RESULT']
 
-  const [resource, setResource] = useState('');
+const Resource = function({resource}) {
+
+  const [name, setName] = useState(resource.name);
+  const [start, setStart] = useState(resource.start);
+  const [input, setInput] = useState(resource.input);
+  const [output, setOutput] = useState(resource.output);
+  const [days, setDays] = useState(resource.days);
+  const [result, setResult] = useState(start);
+
+  useEffect(() => {
+    setStart(result);
+  }, [result])
+
+  const targets = {
+    NAME: setName,
+    START: setStart,
+    INPUT: setInput,
+    OUTPUT: setOutput,
+    DAYS: setDays,
+    RESULT: setResult
+  }
+
   const handleChange = (event) => {
-
+    let name = event.target.name;
+    targets[name](parseInt(event.target.value));
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    targets[RESULT](
+      start + (input * days) - (output * days)
+    );
   };
 
   return (
     <div>
-      <form>
-        <label>Resource: </label><input type="text"></input>
-        <label>Input: </label><input type="text"></input>
-        <label>Output: </label><input type="text"></input>
-        <label>Number of Days: </label><input type="number"></input>
-      <input type="submit" onSubmit={(e) => {handleSubmit(e)}}></input>
+      <form onSubmit={handleSubmit}>
+        <label>Name: </label><span>{name}</span>
+        <label>Starting Value: </label><input type="number" min="0" name={START} value={start} onChange={e => handleChange(e)}></input>
+        <label>Input: </label><input type="number" min="0" name={INPUT} value={input} onChange={e => handleChange(e)}></input>
+        <label>Output: </label><input type="number" min="0" name={OUTPUT} value={output} onChange={e => handleChange(e)}></input>
+        <label>Number of Days: </label><input type="number" min="0" name={DAYS} value={days} onChange={e => handleChange(e)}></input>
+        <input type="submit" value="Calculate"></input>
+        <output name={RESULT} htmlFor={RESOURCE, START, INPUT, OUTPUT, DAYS}>{result}</output>
       </form>
     </div>
   );
--- src/Components/ResourceTracker.jsx
+++ src/Components/ResourceTracker.jsx
@@ -1,15 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Resource from './Resource.jsx';
 
 const ResourceTracker = function() {
+
+  const [resources, setResources] = useState([]);
+  const [newResource, setNewResource] = useState('');
+
+  const handleChange = (event) => {
+    setNewResource(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    var temp = resources.slice();
+    temp.push(
+      {
+        name: newResource,
+        start: 0,
+        input: 0,
+        output: 0,
+        days: 0
+      }
+    );
+    setResources(temp);
+  }
+
+  useEffect(() => {
+
+  }, [resources]);
 
   return (
     <div id="resourcetracker">
       <h2>Resource Tracker</h2>
 
       <div id="trackerArea">
-        <Resource />
+        {resources.map((resource, i) => {
+          return (
+            <Resource resource={resource} key={i}/>
+          )
+        })}
       </div>
+      <form onSubmit={handleSubmit}>
+        <label>Add New Resource: </label>
+        <input type='text' name='newResource' onChange={handleChange}></input>
+        <input type='submit' value="Create"></input>
+        </form>
     </div>
   )
 };
