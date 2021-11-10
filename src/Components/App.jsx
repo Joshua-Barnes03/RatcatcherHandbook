3f8787d54652660f2711f251a9647813a3029848
Joshua Barnes <josh.barnes003@gmail.com>
2021-11-10 03:15:35 -0500

    updated about section to include an about! (and contact info)

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -5,7 +5,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Typography, TextField } from '@mui/material';
 import ResourceTracker from './ResourceTracker.jsx';
 
 const App = function() {
@@ -62,16 +62,42 @@ function Home() {
 }
 
 function About() {
-  return <Box sx={{
-    width: '100%',
-    height: 'auto',
-    position: 'absolute',
-    display: 'flex',
-    justifyContent: 'center',
-    paddingTop: '10px'
-  }}>
-    <Typography variant="h2">About</Typography>
-  </Box>
+  return (
+    <Box id="aboutPage" sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignContent: 'center',
+      position: 'absolute',
+      paddingTop: '10px',
+      color: 'var(--black)'
+    }}>
+      <Typography variant="h2" sx={{position: 'fixed'}} component="h2">About</Typography>
+      <Box sx={{
+        // boxShadow: '0px 1px 3px -1px black',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignContent: 'center',
+        width: '45vw',
+        height: '100%',
+        textAlign: 'center',
+        paddingTop: '10px'
+      }}>
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyingContent: 'center',
+          alignContent: 'center'
+        }}>
+          <Box id="aboutDetails">
+          <Typography variant="body1">Hey, I'm Josh! Thanks for checking out my website. I use it to put up appplications to handle some of the math my players might find at the table when I'm Game Mastering for them. Maybe you'll find it useful too. I'll be adding a variety of different resources over time, both system agnostic and for systems that I use for my games. I'll also be adding some resouces for the tabletop game I'm designing, Ratcatchers, a grit-your-teeth fantasy role playing game about adventurers who risk their life in the mud and dirt for their chance at fame and fortune, or at least enough coin for their next drink.
+
+          <br/><br/>I'm also looking for a job! If you're looking to hire me as a full stack software engineer, you can reach me at josh.barnes@gmail.com!</Typography>
+          </Box>
+        </Box>
+        </ Box>
+    </Box>
+  )
 }
 
 export default App;
